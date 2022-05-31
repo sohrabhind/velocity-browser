d47fe0bdc1ce33fc76aa613e06af80c1aae69fae
sohrabhind <31244889+sohrabhind@users.noreply.github.com>
2022-05-31 14:03:09 +0530

    Update ClearData.java

--- app/src/main/java/com/hindbyte/velocity/intent/ClearData.java
+++ app/src/main/java/com/hindbyte/velocity/intent/ClearData.java
@@ -1,5 +1,6 @@
 package com.hindbyte.velocity.intent;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.webkit.CookieManager;
@@ -7,14 +8,15 @@ import android.webkit.WebStorage;
 import android.webkit.WebViewDatabase;
 
 import com.hindbyte.velocity.activity.BrowserActivity;
+import com.hindbyte.velocity.fragment.BrowserInterface;
 import com.hindbyte.velocity.history.HistoryData;
 import com.hindbyte.velocity.webview.MyWebView;
 
 public class ClearData {
 
-    private final Context context;
+    private final Activity context;
 
-    public ClearData(Context context) {
+    public ClearData(Activity context) {
       this.context = context;
     }
 
@@ -22,27 +24,16 @@ public class ClearData {
         clearCookie();
         HistoryData db = new HistoryData(context);
         db.deleteData();
-        MyWebView mm = new MyWebView(context);
-        mm.clearSslPreferences();
-        mm.clearCache(true);
         WebStorage.getInstance().deleteAllData();
         WebViewDatabase.getInstance(context).clearFormData();
         WebViewDatabase.getInstance(context).clearHttpAuthUsernamePassword();
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
-            WebViewDatabase.getInstance(context).clearUsernamePassword();
-        }
     }
 
     private void clearCookie() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            CookieManager cookieManager = CookieManager.getInstance();
-            cookieManager.flush();
-            cookieManager.removeAllCookies(value -> {
-            });
-        } else {
-            CookieManager cookieManager = CookieManager.getInstance();
-            cookieManager.removeAllCookie();
-        }
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.flush();
+        cookieManager.removeAllCookies(value -> {
+        });
     }
 
     private void deleteAppData() {
